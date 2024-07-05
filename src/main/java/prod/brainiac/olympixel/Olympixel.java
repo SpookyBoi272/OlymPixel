95233615b07a1135456d8d8098321416bf6fda2b
SpookyBoi272 <kattelniranjan@gmail.com>
2024-07-06 00:48:16 +0545

    add Plugin manager

--- src/main/java/prod/brainiac/olympixel/Olympixel.java
+++ src/main/java/prod/brainiac/olympixel/Olympixel.java
@@ -1,7 +1,6 @@
 package prod.brainiac.olympixel;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import prod.brainiac.olympixel.commands.HelpCommand;
 
 public final class Olympixel extends JavaPlugin {
 
@@ -9,7 +8,7 @@ public final class Olympixel extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
-        getCommand("Help").setExecutor(new HelpCommand());
+        System.out.println("Olympixel Started.");
 
     }
 
